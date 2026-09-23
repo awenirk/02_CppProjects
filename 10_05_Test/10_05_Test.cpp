74ac824775c58da043fac4d03cd89a179b082374
Awenirk <avenuslistopad@gmail.com>
2026-09-23 14:39:32 +0300

    some

--- 10_05_Test/10_05_Test.cpp
+++ 10_05_Test/10_05_Test.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int num, i = 1;
+    cout << " " << "Enter Your number: "; cin >> num;
+    cout << " " << "Your num: " << num << endl;
+    do {
+        if (num % 2 == 0) {
+            cout << " " << i << ". " << num << " / 2 = " << num / 2 << endl;
+            num /= 2;
+        }
+        else if (num % 2 != 0) {
+            cout << " " << i << ". (" << num << " * 3 ) + 1 = " << (num * 3) + 1 << endl;
+            num = (num * 3) + 1;
+        }
+        else {
+            cout << "Error!" << endl;
+        }
+        i++;
+    } while (num != 1);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
